32df5583428150c91a9b3936237b52a3e3a569f9
Yenny Hernández <yecrishelo@gmail.com>
2015-03-20 17:35:53 -0500

    Revert "Revert "modificaciones""
    This reverts commit 208e4291a23dcb123271b25aa2d5d193d3180117.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MAUQ</title>
+	<link rel="stylesheet" type="text/css" href="http://localhost/mauq/resources/assets/css/style.css" />
+</head>
+<body>
+		<div class="spacer"> 
+		
+		</div>
+		<div class="adjust">
+			<img src="http://localhost/mauq/resources/assets/images/backDiv.jpg" >
+		</div>
+		<div class="menu">
+			<div class="item">
+				<a href="index.html"><img src="http://localhost/mauq/resources/assets/images/logo.png" alt="Museo de artropodos de la Universidad del Quindio" title="Museo de artropodos de la Universidad del Quindio"></a>
+			</div>
+			<div class="item">
+				<a href="index.html"><img src="http://localhost/mauq/resources/assets/images/visitanos.png" alt="Visitanos" title="Visitanos"></a>
+			</div>
+			<div class="item">
+				<a href="index.html"><img src="http://localhost/mauq/resources/assets/images/explora.png" alt="Explora" title="Explora"></a>
+			</div>
+			<div class="item">
+				<a href="index.html"><img src="http://localhost/mauq/resources/assets/images/areaC.png" alt="Area Cientifica" title="Area Cientifica"></a>
+			</div>
+			<div class="item">
+				<a href="index.html"><img src="http://localhost/mauq/resources/assets/images/somos.png" alt="Quienes somos" title="Quienes Somos"></a>
+			</div>
+		
+		</div>
+		<div class="content">
+		<h1>Bienvenido</h1>
+			</div>
+		<div class="footer">
+		<center>
+			<img src="http://localhost/mauq/resources/assets/images/logo.png" alt="Museo de artrópodos de la Universidad del Quindío" height="50" title="Museo de artropodos de la Universidad del Quindio"></center>
+		</div>
+</body>
+</html>
